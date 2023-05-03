b70f05225a2795b254aae807a493e9a4fd12a723
Munieru <20086673+munierujp@users.noreply.github.com>
2023-05-03 20:04:37 +0900

    Replace markuplint.config.js with markuplint.config.ts

---
+++ markuplint.config.ts
@@ -0,0 +1,13 @@
+import {Config} from '@markuplint/ml-config'
+
+// https://markuplint.dev/docs/configuration
+const config: Config = {
+  extends: [
+    'markuplint:recommended'
+  ],
+  excludeFiles: [
+    './node_modules/**/*'
+  ]
+}
+
+export default config
